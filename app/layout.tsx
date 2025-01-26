8823ba3caa8207a8a18c88d426b58885c307a2d8
José Vitola <jdnv.123@gmail.com>
2025-01-26 14:55:16 -0500

    Initialize navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ApolloWrapper } from "@/lib/apollo/wrapper";
+import { Navbar } from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ApolloWrapper>
-          {children}
+          <div>
+            <Navbar />
+            {children}
+          </div>
         </ApolloWrapper>
       </body>
     </html>
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Alegreya } from 'next/font/google';
+import styles from './Navbar.module.scss';
+
+const alegreya = Alegreya({ subsets: ['latin'] });
+
+export const Navbar = () => {
+    return (
+        <nav style={alegreya.style} className={styles.navbar}>
+            <div className={styles.left}>
+                <a href="/">
+                    Abadía
+                </a>
+            </div>
+        </nav>
+    )
+}
